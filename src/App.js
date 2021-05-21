57016030f07d8210fcbe4493a92bbcdbdb09edcf
thenatural86 <philip.smalls86@gmail.com>
2021-05-21 15:44:46 -0400

    layout and styles

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import NewQuote from './pages/NewQuote'
 import QuoteDetail from './pages/QuoteDetail'
 import Quotes from './pages/Quotes'
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
-    <div>
-      <header></header>
-      <main>
-        <Switch>
-          <Route exact path='/'>
-            <Redirect to='/quotes' />
-          </Route>
-          <Route exact path='/quotes'>
-            <Quotes />
-          </Route>
-          <Route path='/quotes/:quoteId'>
-            <QuoteDetail />
-          </Route>
-          <Route path='/new-quote'>
-            <NewQuote />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Layout>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route exact path='/quotes'>
+          <Quotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   )
 }
 
