d3d855187a498e89870793163a34b86a73d79a42
thenatural86 <philip.smalls86@gmail.com>
2021-05-21 13:27:33 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route, Switch, Redirect } from 'react-router-dom'
+import NewQuote from './pages/NewQuote'
+import QuoteDetail from './pages/QuoteDetail'
+import Quotes from './pages/Quotes'
+
 function App() {
   return (
     <div>
-
+      <header></header>
+      <main>
+        <Switch>
+          <Route exact path='/quotes'>
+            <Quotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+        </Switch>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Quotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quotes = () => {
+  return (
+    <div>
+      <h1>Quotes Page</h1>
+    </div>
+  )
+}
+
+export default Quotes
