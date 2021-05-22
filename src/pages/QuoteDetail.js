b1364cf80a1fef545378355bab08145e3278f8a6
thenatural86 <philip.smalls86@gmail.com>
2021-05-22 13:37:31 -0400

    nested routes to conditionally render

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from 'react-router-dom'
+import { useParams, Route, Link } from 'react-router-dom'
 import Comments from '../components/comments/Comments'
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
 
@@ -20,6 +20,13 @@ const QuoteDetail = () => {
     <>
       <h1>Quote Detail Page</h1>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route exact path={`/quotes/${params.quoteId}`}>
+        <div className='centered'>
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+            Go to Comments
+          </Link>
+        </div>
+      </Route>
       <p>{quote.text}</p>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
