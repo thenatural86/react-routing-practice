06b1e666f43faeab3e913a60e0a94e458eb7e099
thenatural86 <philip.smalls86@gmail.com>
2021-05-23 11:14:21 -0400

    http request to add quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-
+import useHttp from '../hooks/use-http'
+import { addQuote } from '../lib/api'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
   const history = useHistory()
+  const { sendRequest, status } = useHttp(addQuote)
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes')
+    }
+  }, [status, history])
 
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
+    sendRequest(quoteData)
+
     history.push('/quotes')
   }
   return (
